24c6c0c4ce39301067a8db31ed68ae54905b167b
Renato <37927709+ReLoia@users.noreply.github.com>
2022-09-29 22:59:35 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,6 @@
 
 const div = document.createElement("div");
 div.innerHTML = `
-<h1 style="color: red; font-weight: red; font-size: 6em; background: black; position: fixed; top: 0; left: 0; width: 100%; height: 100%; text-align: center;">You got hacked!!111!!!111!</h1>
+<h1 style="color: red; font-weight: red; font-size: 6em; background: black; position: fixed; top: 0; left: 0; width: 100%; height: 100%; text-align: center;">Primo utente registrato!<sub>Questa è una vulnerabilità che ho trovato e sistemato</sub></h1>
 `
 document.body.appendChild(div)

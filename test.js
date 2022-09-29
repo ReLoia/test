6ed843b81caaec1422ebfadb5511f51abdddcddf
Renato <37927709+ReLoia@users.noreply.github.com>
2022-09-29 23:05:13 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 
 const div = document.createElement("div");
 div.innerHTML = `
-<h1 style="color: red; font-weight: red; font-size: 6em; background: black; position: fixed; top: 0; left: 0; width: 100%; height: 100%; text-align: center;">Primo utente registrato!<br>
+<h1 style="color: red; font-weight: red; font-size: 5em; background: black; position: fixed; top: 0; left: 0; width: 100%; height: 100%; text-align: center; margin-block: auto; display: block;">Primo utente registrato!<br>
 Questa è una vulnerabilità che ho trovato e sistemato</h1>
 `
 document.body.appendChild(div)

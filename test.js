a7cd8250e56c2613f6973465654175c42251867b
Renato <37927709+ReLoia@users.noreply.github.com>
2022-09-29 21:48:39 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-alert("Test")
+// Just a test for a vulnerability I've found on my website.
+
+const div = document.createElement("div");
+div.innerHTML = `
+<h1 style="color: red; font-weight: red; font-size: 6em">You got hacked!!111!!!111!</h1>
+`
